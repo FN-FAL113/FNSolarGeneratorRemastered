fbcc77e2c4e43db5c9e709c1d0f2890ab5754b19
FN-FAL113 <88238718+FN-FAL113@users.noreply.github.com>
2021-10-09 08:41:18 +0800

    Configurable Lore, Day Rate and Night Rate

--- FN_FAL-SolarGenerators/src/main/java/me/fnfal113/fn_falsolargenerators/FN_FALSolarGenerators.java
+++ FN_FAL-SolarGenerators/src/main/java/me/fnfal113/fn_falsolargenerators/FN_FALSolarGenerators.java
@@ -12,6 +12,7 @@ import io.github.thebusybiscuit.slimefun4.libraries.dough.skins.PlayerHead;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.skins.PlayerSkin;
 import io.github.thebusybiscuit.slimefun4.utils.LoreBuilder;
 import me.fnfal113.fn_falsolargenerators.Generators.*;
+import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
@@ -30,115 +31,115 @@ public class FN_FALSolarGenerators extends JavaPlugin implements SlimefunAddon {
         config.options().copyDefaults();
         saveDefaultConfig();
 
-        SolarGeneratorCustom solarGenerator1 = registerFNGeneratorTier1(itemGroup, "FN_FAL_GENERATOR_TIER1", "6d2822cedb3abd579d6dfa2966c1433c3c36cb9732e2c23ec0cc81daedd4403b", "&dFN Solar Generator Tier I", config.getInt("Tier1-power1"), config.getInt("Buffer1"), new ItemStack[] {
+        SolarGeneratorCustom solarGenerator1 = registerFNGeneratorTier1(itemGroup, "FN_FAL_GENERATOR_TIER1", "6d2822cedb3abd579d6dfa2966c1433c3c36cb9732e2c23ec0cc81daedd4403b", "&dFN Solar Generator Tier I", config.getInt("Tier1-power1"), config.getInt("Tier1-power1-night"), config.getInt("Buffer1"), new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_3, SlimefunItems.SOLAR_GENERATOR_4 , SlimefunItems.SOLAR_GENERATOR_3,
                 SlimefunItems.MEDIUM_CAPACITOR, SlimefunItems.POWER_CRYSTAL, SlimefunItems.MEDIUM_CAPACITOR,
                 SlimefunItems.MEDIUM_CAPACITOR, SlimefunItems.SOLAR_GENERATOR_4, SlimefunItems.MEDIUM_CAPACITOR
         });
 
-        SolarGeneratorCustom solarGenerator2 = registerFNGeneratorTier2(itemGroup, "FN_FAL_GENERATOR_TIER2", "35dd37f729fc88133e314a552204c0fa2c0168428b353f957bf15ff24b7707e0", "&dFN Solar Generator Tier II", config.getInt("Tier2-power2"), config.getInt("Buffer2"), new ItemStack[] {
+        SolarGeneratorCustom solarGenerator2 = registerFNGeneratorTier2(itemGroup, "FN_FAL_GENERATOR_TIER2", "35dd37f729fc88133e314a552204c0fa2c0168428b353f957bf15ff24b7707e0", "&dFN Solar Generator Tier II", config.getInt("Tier2-power2"), config.getInt("Tier2-power2-night"), config.getInt("Buffer2"), new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_4, solarGenerator1.getItem() , SlimefunItems.SOLAR_GENERATOR_4,
                 SlimefunItems.BIG_CAPACITOR, SlimefunItems.POWER_CRYSTAL, SlimefunItems.BIG_CAPACITOR,
                 SlimefunItems.BIG_CAPACITOR, solarGenerator1.getItem(), SlimefunItems.BIG_CAPACITOR
         });
 
-        SolarGeneratorCustom solarGenerator3 = registerFNGeneratorTier3(itemGroup, "FN_FAL_GENERATOR_TIER3", "25ddf6af2d6271d8fdfadbdc54faaad5a68d7b8ac20e163883fc38d76336ea6", "&dFN Solar Generator Tier III", config.getInt("Tier3-power3"), config.getInt("Buffer3"), new ItemStack[] {
+        SolarGeneratorCustom solarGenerator3 = registerFNGeneratorTier3(itemGroup, "FN_FAL_GENERATOR_TIER3", "25ddf6af2d6271d8fdfadbdc54faaad5a68d7b8ac20e163883fc38d76336ea6", "&dFN Solar Generator Tier III", config.getInt("Tier3-power3"), config.getInt("Tier3-power3-night"), config.getInt("Buffer3"), new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_4 , solarGenerator2.getItem() , SlimefunItems.SOLAR_GENERATOR_4,
                 solarGenerator1.getItem(), SlimefunItems.POWER_CRYSTAL, solarGenerator1.getItem(),
                 SlimefunItems.SOLAR_GENERATOR_3, SlimefunItems.LARGE_CAPACITOR, SlimefunItems.SOLAR_GENERATOR_3
         });
 
-        SolarGeneratorCustom solarGenerator4 = registerFNGeneratorTier4(itemGroup, "FN_FAL_GENERATOR_TIER4", "e7f4c00356d1addb85b45ba5352992d3ecc0c9d11feb9041482f8531fd27d014", "&dFN Solar Generator Tier IV", config.getInt("Tier4-power4"), config.getInt("Buffer4"), new ItemStack[] {
+        SolarGeneratorCustom solarGenerator4 = registerFNGeneratorTier4(itemGroup, "FN_FAL_GENERATOR_TIER4", "e7f4c00356d1addb85b45ba5352992d3ecc0c9d11feb9041482f8531fd27d014", "&dFN Solar Generator Tier IV", config.getInt("Tier4-power4"), config.getInt("Tier4-power4-night"), config.getInt("Buffer4"), new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_4 , solarGenerator3.getItem() , SlimefunItems.SOLAR_GENERATOR_4,
                 SlimefunItems.BIG_CAPACITOR, SlimefunItems.POWER_CRYSTAL, SlimefunItems.BIG_CAPACITOR,
                 solarGenerator1.getItem(), SlimefunItems.LARGE_CAPACITOR, solarGenerator1.getItem()
         });
 
-        SolarGeneratorCustom solarGenerator5 = registerFNGeneratorTier5(itemGroup, "FN_FAL_GENERATOR_TIER5", "afdd9e588d2461d2d3d058cb3e0af2b3a3367607aa14d124ed92a833f25fb112", "&dFN Solar Generator Tier V", config.getInt("Tier5-power5"), config.getInt("Buffer5"), new ItemStack[] {
+        SolarGeneratorCustom solarGenerator5 = registerFNGeneratorTier5(itemGroup, "FN_FAL_GENERATOR_TIER5", "afdd9e588d2461d2d3d058cb3e0af2b3a3367607aa14d124ed92a833f25fb112", "&dFN Solar Generator Tier V", config.getInt("Tier5-power5"), config.getInt("Tier5-power5-night"),config.getInt("Buffer5"), new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_4 , solarGenerator4.getItem() , SlimefunItems.SOLAR_GENERATOR_4,
                 solarGenerator1.getItem(), solarGenerator2.getItem(), solarGenerator1.getItem(),
                 SlimefunItems.LARGE_CAPACITOR, SlimefunItems.LARGE_CAPACITOR, SlimefunItems.LARGE_CAPACITOR
         });
 
-        SolarGeneratorCustom solarGenerator6 = registerFNGeneratorTier6(itemGroup, "FN_FAL_GENERATOR_TIER6", "224ad26209fa02f559ef6aa863ee9ba8f3bef0a02f1e9cff8fdc09196402fb6f", "&dFN Solar Generator Tier VI", config.getInt("Tier6-power6"), config.getInt("Buffer6"), new ItemStack[] {
+        SolarGeneratorCustom solarGenerator6 = registerFNGeneratorTier6(itemGroup, "FN_FAL_GENERATOR_TIER6", "224ad26209fa02f559ef6aa863ee9ba8f3bef0a02f1e9cff8fdc09196402fb6f", "&dFN Solar Generator Tier VI", config.getInt("Tier6-power6"), config.getInt("Tier6-power6-night"),config.getInt("Buffer6"), new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_4 , solarGenerator5.getItem() , SlimefunItems.SOLAR_GENERATOR_4,
                 SlimefunItems.BOOSTED_URANIUM, SlimefunItems.POWER_CRYSTAL, SlimefunItems.BOOSTED_URANIUM,
                 solarGenerator3.getItem(), SlimefunItems.LARGE_CAPACITOR, solarGenerator3.getItem()
         });
 
-        SolarGeneratorCustom solarGenerator7 = registerFNGeneratorTier7(itemGroup, "FN_FAL_GENERATOR_TIER7", "c4fe135c311f7086edcc5e6dbc4ef4b23f819fddaa42f827dac46e3574de2287", "&dFN Solar Generator Tier VII", config.getInt("Tier7-power7"), config.getInt("Buffer7"), new ItemStack[] {
+        SolarGeneratorCustom solarGenerator7 = registerFNGeneratorTier7(itemGroup, "FN_FAL_GENERATOR_TIER7", "c4fe135c311f7086edcc5e6dbc4ef4b23f819fddaa42f827dac46e3574de2287", "&dFN Solar Generator Tier VII", config.getInt("Tier7-power7"), config.getInt("Tier7-power7-night"), config.getInt("Buffer7"), new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_4 , solarGenerator6.getItem() , SlimefunItems.SOLAR_GENERATOR_4,
                 SlimefunItems.LARGE_CAPACITOR, SlimefunItems.POWER_CRYSTAL, SlimefunItems.LARGE_CAPACITOR,
                 SlimefunItems.CARBONADO_EDGED_CAPACITOR, solarGenerator3.getItem(), SlimefunItems.CARBONADO_EDGED_CAPACITOR
         });
 
-        SolarGeneratorCustom solarGenerator8 = registerFNGeneratorTier8(itemGroup, "FN_FAL_GENERATOR_TIER8", "240775c3ad75763613f32f04986881bbe4eee4366d0c57f17f7c7514e2d0a77d", "&dFN Solar Generator Tier VIII", config.getInt("Tier8-power8"), config.getInt("Buffer8"),  new ItemStack[] {
+        SolarGeneratorCustom solarGenerator8 = registerFNGeneratorTier8(itemGroup, "FN_FAL_GENERATOR_TIER8", "240775c3ad75763613f32f04986881bbe4eee4366d0c57f17f7c7514e2d0a77d", "&dFN Solar Generator Tier VIII", config.getInt("Tier8-power8"), config.getInt("Tier8-power8-night"), config.getInt("Buffer8"),  new ItemStack[] {
                 SlimefunItems.SOLAR_GENERATOR_4 , solarGenerator7.getItem() , SlimefunItems.SOLAR_GENERATOR_4,
                 SlimefunItems.BOOSTED_URANIUM, SlimefunItems.POWER_CRYSTAL, SlimefunItems.BOOSTED_URANIUM,
                 SlimefunItems.CARBONADO_EDGED_CAPACITOR, solarGenerator3.getItem(), SlimefunItems.CARBONADO_EDGED_CAPACITOR
         });
     }
 
-    private SolarGeneratorCustom registerFNGeneratorTier1(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer1")), LoreBuilder.powerPerSecond(power * 2));
+    private SolarGeneratorCustom registerFNGeneratorTier1(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier1-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer1")), LoreBuilder.powerPerSecond(dayEnergy * 2));
 
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
         generator.register(this);
         return generator;
     }
 
-    private SolarGeneratorCustom registerFNGeneratorTier2(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer2")), LoreBuilder.powerPerSecond(power * 2));
+    private SolarGeneratorCustom registerFNGeneratorTier2(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier2-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer2")), LoreBuilder.powerPerSecond(dayEnergy * 2));
 
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
         generator.register(this);
         return generator;
     }
 
-    private SolarGeneratorCustom registerFNGeneratorTier3(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer3")), LoreBuilder.powerPerSecond(power * 2));
+    private SolarGeneratorCustom registerFNGeneratorTier3(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier3-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer3")), LoreBuilder.powerPerSecond(dayEnergy * 2));
 
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
         generator.register(this);
         return generator;
     }
 
-    private SolarGeneratorCustom registerFNGeneratorTier4(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer4")), LoreBuilder.powerPerSecond(power * 2));
+    private SolarGeneratorCustom registerFNGeneratorTier4(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier4-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer4")), LoreBuilder.powerPerSecond(dayEnergy * 2));
 
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
         generator.register(this);
         return generator;
     }
 
-    private SolarGeneratorCustom registerFNGeneratorTier5(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer5")), LoreBuilder.powerPerSecond(power * 2));
+    private SolarGeneratorCustom registerFNGeneratorTier5(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier5-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer5")), LoreBuilder.powerPerSecond(dayEnergy * 2));
 
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
         generator.register(this);
         return generator;
     }
 
-    private SolarGeneratorCustom registerFNGeneratorTier6(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer6")), LoreBuilder.powerPerSecond(power * 2));
+    private SolarGeneratorCustom registerFNGeneratorTier6(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier6-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer6")), LoreBuilder.powerPerSecond(dayEnergy * 2));
 
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
         generator.register(this);
         return generator;
     }
 
-    private SolarGeneratorCustom registerFNGeneratorTier7(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer7")), LoreBuilder.powerPerSecond(power * 2));
+    private SolarGeneratorCustom registerFNGeneratorTier7(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier7-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer7")), LoreBuilder.powerPerSecond(dayEnergy * 2));
 
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
         generator.register(this);
         return generator;
     }
 
 
-    private SolarGeneratorCustom registerFNGeneratorTier8(ItemGroup itemGroup, String id, String texture, String name, int power, int capacity, ItemStack[] recipe) {
-        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", "&eGenerator works Day and Night", "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer8")), LoreBuilder.powerPerSecond(power * 2));
-        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup,power, power, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
+    private SolarGeneratorCustom registerFNGeneratorTier8(ItemGroup itemGroup, String id, String texture, String name, int dayEnergy, int nightEnergy, int capacity, ItemStack[] recipe) {
+        SlimefunItemStack item = new SlimefunItemStack(id, texture, name, "", "&eA solar generator for those who needs power", (ChatColor.translateAlternateColorCodes(('&'), config.getString("Tier8-Lore"))), "", LoreBuilder.machine(MachineTier.END_GAME, MachineType.GENERATOR), LoreBuilder.powerBuffer(config.getInt("Buffer8")), LoreBuilder.powerPerSecond(dayEnergy * 2));
+        SolarGeneratorCustom generator = new SolarGeneratorCustom(itemGroup, dayEnergy, nightEnergy, item, RecipeType.ENHANCED_CRAFTING_TABLE, recipe, capacity);
 
         generator.register(this);
         return generator;
